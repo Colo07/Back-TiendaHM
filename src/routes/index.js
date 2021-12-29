4d6161f8e280ace0ffb9a07b2cf9aff4e1918b9d
Colo07 <fergardenes7@gmail.com>
2021-12-29 18:07:58 -0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,9 @@ const hoodiesRouter = require ("./hoodiesRouter");
 const tshirtsRouter = require ("./tshirtsRouter");
 const mugsRouter = require ("./mugsRouter");
 
-const hoodiesController = require ("../controllers/hoodiesController");
-const tshirtsController = require ("../controllers/tshirtsController");
-const mugsController = require ("../controllers/mugsController")
+// const hoodiesController = require ("../controllers/hoodiesController");
+// const tshirtsController = require ("../controllers/tshirtsController");
+// const mugsController = require ("../controllers/mugsController")
 const router = express.Router();
 router.use("/hoodies",hoodiesRouter);
 router.use("/tshirts",tshirtsRouter);
@@ -15,11 +15,11 @@ router.use("/mugs",mugsRouter);
 // router.get("/hoodies",hoodiesController.getHoodies) ;
 // router.get ("/tshirts", tshirtsController.getTshirts);
 // router.get ("/mugs", mugsController.getMugs);
-router.get("/",hoodiesController.getHoodies) ;
+// router.get("/",hoodiesController.getHoodies) ;
 // router.get ("/", tshirtsController.getTshirts);
 // router.get ("/", mugsController.getMugs);
-router.get("/:id",hoodiesController.getHoodiesById) ;
-router.get ("/:id", tshirtsController.getTshirtsById);
+// router.get("/:id",hoodiesController.getHoodiesById) ;
+// router.get ("/:id", tshirtsController.getTshirtsById);
 
 
 module.exports=router;  
